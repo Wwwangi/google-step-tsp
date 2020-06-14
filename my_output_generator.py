8c388851b643f45aaefcbba6e1fadec17f1dc372
Wangi <wwwangi@moegi.waseda.jp>
2020-06-14 21:01:16 +0900

    Greedy + sa + intersection detection

--- my_output_generator.py
+++ my_output_generator.py
@@ -5,15 +5,15 @@ from common import format_tour, read_input
 import solver_greedy
 import solver_random
 import solver_dp
-import solver_greedy_intersection
+import solver_sa
 
 CHALLENGES = 4
 
 
 def generate_sample_output():
-    for i in range(5,6):
+    for i in range(6,7):
         cities = read_input(f'input_{i}.csv')
-        solver = solver_greedy_intersection
+        solver = solver_sa
         name = 'output'
         tour = solver.solve(cities)
         with open(f'{name}_{i}.csv', 'w') as f:
